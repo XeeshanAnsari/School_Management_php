ef1a95ae98bcd4696f610cbc4ad8e3417a4c044e
zeeshan <zeeshanansari38244@gmail.com>
2016-09-08 22:34:01 -0700

    create admin and regShowRecord page for display Registration Data

---
+++ admin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/jquery.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+    
+        <!--  NavBar is start here-->
+     <div class="container-fuild"> 
+        <nav class="navbar navbar-default  ">
+            <div class="container-fluid">
+                <div class="navbar-brand">
+                    Admin Penal
+                </div>
+                <div class="navbar-header navbar-right">
+                  <ul class="nav navbar-nav  ">
+                      <li><a href="regShowRecord.php"> ShowRegistrationForm</a></li>
+                  </ul>
+                </div>
+                
+            </div>  
+        </nav>
+     </div>    
+        <!-- Navbar is end here -->
+        
+</body>
+</html>
