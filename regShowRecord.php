496af19a92ad0ad8691808d37a7bf9b76de93df8
zeeshan <zeeshanansari38244@gmail.com>
2016-09-08 22:49:04 -0700

    All registration record show in regShowRecord page using table

--- regShowRecord.php
+++ regShowRecord.php
@@ -20,7 +20,7 @@
                 </div>
                 <div class="navbar-header navbar-right">
                   <ul class="nav navbar-nav  ">
-                      <li><a href="regShowRecord.php"> ShowRegistrationForm</a></li>
+                      <li><a href="regShowRecord.php"> ShowRegistration</a></li>
                   </ul>
                 </div>
                 
@@ -31,7 +31,7 @@
         
         <!-- Show Registration Record -->
         <div class="container">
-            <table class="table table-default">
+            <table class="table table-default table-hover">
                 <tr>
                     <th>Roll Number</th>
                     <th>Student Name</th>
@@ -41,6 +41,33 @@
                     <th>Email Address</th>
                     <th>Address</th>   
                 </tr>
+                
+                
+ <?php
+     include("./database/db_connect.php");
+     $query = "SELECT * FROM registration";
+     $run = mysql_query($query);
+     while ($row=mysql_fetch_array($run)) {
+         $id = $row['g_r_no'];
+         $name = $row['s_name'];
+         $fname = $row['s_father_name'];
+         $class = $row['s_class'];
+         $phone = $row['s_phone'];
+         $email = $row['s_email'];
+         $address = $row['s_address'];
+      
+ ?>               
+                <tr>
+                   <td><?php echo $id;  ?></td>
+                   <td><?php echo $name;  ?></td>
+                   <td><?php echo $fname;  ?></td>
+                   <td><?php echo $class;  ?></td>
+                   <td><?php echo $phone;  ?></td>
+                   <td><?php echo $email;  ?></td>
+                   <td><?php echo $address;  ?></td>
+                                  
+                </tr>
+<?php  } ?>                
             </table>
         </div>
         
